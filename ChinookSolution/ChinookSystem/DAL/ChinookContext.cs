9afc55205402d163180f3f3b31c2eb5fc0f650c8
Echo <1114121831@qq.com>
2017-05-15 09:40:47 -0700

    End of

---
+++ ChinookSolution/ChinookSystem/DAL/ChinookContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#region Additional Namespaces
+using System.Data.Entity;
+#endregion 
+
+namespace ChinookSystem.DAL
+{
+    //This is an internal class for security reasons
+    //Access is restricted to within this class library project
+    //Inherits DbContext for Entity Framework, which requries
+    // System.Data.Entity
+    internal class ChinookContext : DbContext
+    {
+        //Pass the connection string name to the
+        //DbContext using :base()
+        //i.e.connection string name is "ChinookDB"
+        public ChinookContext() : base("ChinookDB")
+        {
+
+        }
+            
+    }
+}
